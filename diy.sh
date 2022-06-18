a21c74c79cc67d2f785b75f95162bb6bd0ac994a
Ing <wjz304@qq.com>
2022-06-19 01:21:09 +0800

    init

--- diy.sh
+++ diy.sh
@@ -1,11 +1,11 @@
 
 
 # 卸载 luci/themes/luci-theme-argon 旧版本
-./scripts/feeds uninstall luci-theme-argon
+#./scripts/feeds uninstall luci-theme-argon
 # 卸载 luci/luci-app-netdata 旧版本
-./scripts/feeds uninstall luci-app-netdata
+#./scripts/feeds uninstall luci-app-netdata
 # 安装新版本
-./scripts/feeds install -a -f -p packagesing
+#./scripts/feeds install -a -f -p ing
 
 # 修改默认管理地址
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
