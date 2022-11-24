8735dc943b2b96f46aa71733af4994cc028d382b
Ing <wjz304@qq.com>
2022-11-24 15:14:31 +0800

    tar release

--- diy.sh
+++ diy.sh
@@ -82,9 +82,14 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
 
 
-# Modify rootfs size
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=256/' .config
+# Modify kernel and rootfs size
+sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=.*$/CONFIG_TARGET_KERNEL_PARTSIZE=64/' .config
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=2048/' .config
 
+# Modify Default PPPOE Setting
+#sed -i '$i uci set network.wan.username=PPPOE_USERNAME' openwrt/package/*/*/my-default-settings/files/etc/uci-defaults/95-default-settings
+#sed -i '$i uci set network.wan.password=PPPOE_PASSWD' openwrt/package/*/*/my-default-settings/files/etc/uci-defaults/95-default-settings
+#sed -i '$i uci commit network' openwrt/package/*/*/my-default-settings/files/etc/uci-defaults/95-default-settings
 
 # Modify app list
 sed -i 's/"vpn"/"services"/g; s/"VPN"/"Services"/g' package/feeds/luci/luci-app-ipsec-server/luasrc/controller/ipsec-server.lua    # `grep "IPSec VPN Server" -rl ./`
