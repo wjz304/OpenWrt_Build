83e0853a494d4a25f04481740a1a4a5b6a95d19f
Ing <wjz304@qq.com>
2024-11-29 16:57:09 +0800

    update

--- build.sh
+++ build.sh
@@ -6,15 +6,20 @@
 # See /LICENSE for more information.
 #
 
-WORKSPACE=${GITHUB_WORKSPACE:-$(pwd)}
-release="test"
-script="diy.sh"
-config="${1}"                 # config 命名规则: <repo>;<owner>;<name>.config
-CONFIG=${config/.config/}     # 去掉 .config 后缀
-CONFIG_ARRAY=(${config//;/ }) # ;号分割
+if [ -z "${1}" ] || [ ! -f "${1}" ]; then
+  echo "Usage: $0 <config file>"
+  exit 1
+fi
+
+WORKSPACE="$(pwd)"
+
+script_path=$(realpath "$(dirname "${1}")/diy.sh")
+config_path=$(realpath "${1}")               # 绝对路径
+CONFIG_FNAME=$(basename "${1}" .config) # 取文件名
+CONFIG_ARRAY=(${CONFIG_FNAME//;/ })     # 分割成数组
 
 if [ ${#CONFIG_ARRAY[@]} -ne 3 ]; then
-  echo "${config} name error!"
+  echo "${config_path} name error!" # config 命名规则: <repo>;<owner>;<name>.config
   exit 1
 fi
 
@@ -22,18 +27,19 @@ CONFIG_REPO="${CONFIG_ARRAY[0]}"
 CONFIG_OWNER="${CONFIG_ARRAY[1]}"
 CONFIG_NAME="${CONFIG_ARRAY[2]}"
 
-if [ "${CONFIG_REPO}" == "openwrt" ]; then
+if [ "${CONFIG_REPO}" = "openwrt" ]; then
   REPO_URL="https://github.com/openwrt/openwrt"
   REPO_BRANCH="master"
-fi
-
-if [ "${CONFIG_REPO}" == "lede" ]; then
+elif [ "${CONFIG_REPO}" = "lede" ]; then
   REPO_URL="https://github.com/coolsnowwolf/lede"
   REPO_BRANCH="master"
+else
+  echo "${config_path} name error!"
+  exit 1
 fi
 
-if [ ! -d ${CONFIG_REPO} ]; then
-  git clone --depth=1 -b ${REPO_BRANCH} ${REPO_URL} ${CONFIG_REPO}
+if [ ! -d "${CONFIG_REPO}" ]; then
+  git clone --depth=1 -b "${REPO_BRANCH}" "${REPO_URL}" "${CONFIG_REPO}"
   # if [ -d "${CONFIG_REPO}/package/kernel/r8125" ]; then
   #   rm -rf ${CONFIG_REPO}/package/kernel/r8125
   # fi
@@ -45,7 +51,8 @@ fi
 # root.
 export FORCE_UNSAFE_CONFIGURE=1
 
-pushd ${CONFIG_REPO}
+pushd "${CONFIG_REPO}"
+
 git pull
 
 sed -i "/src-git ing /d; 1 i src-git ing https://github.com/wjz304/openwrt-packages;${CONFIG_REPO}" feeds.conf.default
@@ -59,19 +66,19 @@ sed -i "/src-git ing /d; 1 i src-git ing https://github.com/wjz304/openwrt-packa
 ./scripts/feeds uninstall $(grep Package ./feeds/ing.index | awk -F': ' '{print $2}')
 ./scripts/feeds install -p ing -a
 
-cp -f "${WORKSPACE}/${config}" "./.config"
-cp -f "${WORKSPACE}/${script}" "./diy.sh"
+cp -f "${config_path}" "./.config"
+cp -f "${script_path}" "./diy.sh"
 
 chmod +x "./diy.sh"
 "./diy.sh" "${CONFIG_REPO}" "${CONFIG_OWNER}"
 
 make defconfig
 
-if [ "$GITHUB_ACTIONS" == "true" ]; then
-  pushd ${WORKSPACE}
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  pushd "${GITHUB_WORKSPACE}"
   git pull
-  cp -f ${WORKSPACE}/${CONFIG_REPO}/.config "${WORKSPACE}/${config}"
-  status=$(git status -s | grep "${config}" | awk '{printf $2}')
+  cp -f "${WORKSPACE}/${CONFIG_REPO}/.config" "${GITHUB_WORKSPACE}/${config}"
+  status=$(git status -s | grep "${CONFIG_FNAME}" | awk '{printf $2}')
   if [ -n "${status}" ]; then
     git add "${status}"
     git commit -m "update $(date +%Y-%m-%d" "%H:%M:%S)"
@@ -80,32 +87,35 @@ if [ "$GITHUB_ACTIONS" == "true" ]; then
   popd
 fi
 
-echo -e "download package"
+echo "download package"
 make download -j8 V=s
 
-# find dl -size -1024c -exec ls -l {} \;
-# find dl -size -1024c -exec rm -f {} \;
+# find dl -size -1024c -exec ls -l {} \; -exec rm -f {} \;
 
-echo -e "$(nproc) thread compile"
+echo "$(nproc) thread compile"
 make -j$(nproc) V=s || make -j1 V=s
-
-DEVICE_NAME=$(grep '^CONFIG_TARGET.*DEVICE.*=y' .config | sed -r 's/.*DEVICE_(.*)=y/\1/')
+if [ $? -ne 0 ]; then
+  echo "Build failed!"
+  popd # ${CONFIG_REPO}
+  exit 1
+fi
 
 pushd bin/targets/*/*
+
 ls -al
 
 # sed -i '/buildinfo/d; /\.bin/d; /\.manifest/d' sha256sums
 rm -rf packages *.buildinfo *.manifest *.bin sha256sums
 
-filename=${CONFIG_REPO}-${CONFIG_NAME}-${release}.zip
+rm -f *.img.gz
+gzip -f *.img
 
-gzip *.img 2>/dev/null || true
-zip -q -r ${filename} *
+mv -f *.img.gz "${WORKSPACE}"
 
-if [ "$GITHUB_ACTIONS" == "true" ]; then
-  echo "firmware=$(pwd)/${filename}" >>$GITHUB_ENV
-else
-  mv -f ${filename} ${WORKSPACE}
-fi
 popd # bin/targets/*/*
+
 popd # ${CONFIG_REPO}
+
+du -chd1 "${CONFIG_REPO}"
+
+echo "Done"
