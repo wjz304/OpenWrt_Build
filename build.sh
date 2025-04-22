6bc2ee74172029abf00749cf6a4504dc8ee7eedc
Ing <wjz304@qq.com>
2025-04-22 21:18:20 +0800

    更新构建脚本，优化依赖项和配置处理

--- build.sh
+++ build.sh
@@ -11,21 +11,24 @@ if [ -z "${1}" ] || [ ! -f "${1}" ]; then
   exit 1
 fi
 
-WORKSPACE="$(pwd)"
+WORK_PATH="$(pwd)"
 
-script_path=$(realpath "$(dirname "${1}")/diy.sh")
-config_path=$(realpath "${1}")               # 绝对路径
-CONFIG_FNAME=$(basename "${1}" .config) # 取文件名
-CONFIG_ARRAY=(${CONFIG_FNAME//;/ })     # 分割成数组
+SCRIPT_FILE="${WORK_PATH}/diy.sh"
+CONFIG_FILE=$(realpath "${1}")                        # 传入的配置文件
+CONFIG_PATH=$(dirname "${CONFIG_FILE}")               # 配置文件路径
+CONFIG_NAME=$(basename "${CONFIG_FILE}" .config)      # 配置文件名
+IFS=';' read -r -a CONFIG_ARRAY <<< "${CONFIG_NAME}"  # 分割配置文件名
+
+GITHUB_ACTIONS="${2:-false}"
 
 if [ ${#CONFIG_ARRAY[@]} -ne 3 ]; then
-  echo "${config_path} name error!" # config 命名规则: <repo>;<owner>;<name>.config
+  echo "${CONFIG_FILE} name error!" # config 命名规则: <repo>;<owner>;<name>.config
   exit 1
 fi
 
 CONFIG_REPO="${CONFIG_ARRAY[0]}"
 CONFIG_OWNER="${CONFIG_ARRAY[1]}"
-CONFIG_NAME="${CONFIG_ARRAY[2]}"
+CONFIG_ARCH="${CONFIG_ARRAY[2]}"
 
 if [ "${CONFIG_REPO}" = "openwrt" ]; then
   REPO_URL="https://github.com/openwrt/openwrt"
@@ -34,12 +37,12 @@ elif [ "${CONFIG_REPO}" = "lede" ]; then
   REPO_URL="https://github.com/coolsnowwolf/lede"
   REPO_BRANCH="master"
 else
-  echo "${config_path} name error!"
+  echo "${CONFIG_FILE} name error!"
   exit 1
 fi
 
-if [ ! -d "${CONFIG_REPO}" ]; then
-  git clone --depth=1 -b "${REPO_BRANCH}" "${REPO_URL}" "${CONFIG_REPO}"
+if [ ! -d "${WORK_PATH}/${CONFIG_REPO}" ]; then
+  git clone --depth=1 -b "${REPO_BRANCH}" "${REPO_URL}" "${WORK_PATH}/${CONFIG_REPO}"
   # if [ -d "${CONFIG_REPO}/package/kernel/r8125" ]; then
   #   rm -rf ${CONFIG_REPO}/package/kernel/r8125
   # fi
@@ -51,7 +54,7 @@ fi
 # root.
 export FORCE_UNSAFE_CONFIGURE=1
 
-pushd "${CONFIG_REPO}"
+pushd "${WORK_PATH}/${CONFIG_REPO}" || exit
 
 git pull
 
@@ -63,28 +66,29 @@ sed -i "/src-git ing /d; 1 i src-git ing https://github.com/wjz304/openwrt-packa
 #   git clone --depth=1 -b 22.x https://github.com/sbwml/packages_lang_golang ./feeds/packages/lang/golang
 # fi
 ./scripts/feeds install -a
-./scripts/feeds uninstall $(grep Package ./feeds/ing.index | awk -F': ' '{print $2}')
+./scripts/feeds uninstall "$(grep Package ./feeds/ing.index 2>/dev/null | awk -F': ' '{print $2}')"
 ./scripts/feeds install -p ing -a
 
-cp -f "${config_path}" "./.config"
-cp -f "${script_path}" "./diy.sh"
+cp -f "${CONFIG_FILE}" "./.config"
+cp -f "${SCRIPT_FILE}" "./diy.sh"
 
 chmod +x "./diy.sh"
-"./diy.sh" "${CONFIG_REPO}" "${CONFIG_OWNER}"
+"./diy.sh" "${WORK_PATH}/${CONFIG_REPO}" "${CONFIG_OWNER}" "${CONFIG_ARCH}"
 
 make defconfig
 
-if [ "$GITHUB_ACTIONS" = "true" ]; then
-  pushd "${GITHUB_WORKSPACE}"
+if [ "${GITHUB_ACTIONS}" = "true" ]; then
+  echo "upload ${CONFIG_FILE}"
+  pushd "${CONFIG_PATH}" || exit
   git pull
-  cp -f "${WORKSPACE}/${CONFIG_REPO}/.config" "${GITHUB_WORKSPACE}/${config}"
-  status=$(git status -s | grep "${CONFIG_FNAME}" | awk '{printf $2}')
+  cp -vf "${WORK_PATH}/${CONFIG_REPO}/.config" "${CONFIG_FILE}"
+  status=$(git status -s | grep "${CONFIG_NAME}" | awk '{printf $2}')
   if [ -n "${status}" ]; then
     git add "${status}"
     git commit -m "update $(date +%Y-%m-%d" "%H:%M:%S)"
     git push -f
   fi
-  popd
+  popd || exit # "${CONFIG_PATH}"
 fi
 
 echo "download package"
@@ -93,14 +97,14 @@ make download -j8 V=s
 # find dl -size -1024c -exec ls -l {} \; -exec rm -f {} \;
 
 echo "$(nproc) thread compile"
-make -j$(nproc) V=s || make -j1 V=s
+make -j"$(nproc)" V=s || make -j1 V=s
 if [ $? -ne 0 ]; then
   echo "Build failed!"
-  popd # ${CONFIG_REPO}
+  popd || exit # "${WORK_PATH}/${CONFIG_REPO}"
   exit 1
 fi
 
-pushd bin/targets/*/*
+pushd bin/targets/*/* || exit
 
 ls -al
 
@@ -110,12 +114,12 @@ rm -rf packages *.buildinfo *.manifest *.bin sha256sums
 rm -f *.img.gz
 gzip -f *.img
 
-mv -f *.img.gz "${WORKSPACE}"
+mv -f *.img.gz "${WORK_PATH}"
 
-popd # bin/targets/*/*
+popd || exit # bin/targets/*/*
 
-popd # ${CONFIG_REPO}
+popd || exit # "${WORK_PATH}/${CONFIG_REPO}"
 
-du -chd1 "${CONFIG_REPO}"
+du -chd1 "${WORK_PATH}/${CONFIG_REPO}"
 
 echo "Done"
